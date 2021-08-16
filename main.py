10586913dc7b13117c032726c0ac15f929f82482
KristineGray <krisgraya@gmail.com>
2021-08-16 16:40:22 -0400

    completed exercise

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # Import modules here. Use the 'as' keyword to rename 'print_all' as 'display_results'.
- 
-
+import averages
+from display import print_all as display_results
+import random_select
 
 # Candidate data:
 astronauts = ['Fox','Turtle','Cat','Hippo','Dog']
-
 test_titles = ['Math','Fitness','Coding','Nav','Communication']
-
 scores = [[95, 86, 83, 81, 76],[79, 71, 79, 87, 72],[94, 87, 87, 83, 82],[99, 77, 91, 79, 80],[96, 95, 99, 82, 70]]
 
 # User interface:
@@ -17,17 +16,22 @@ for index in range(len(prompts)):
   if response.lower() == 'y':
     if index == 0:
       # Call 'display_results' here and pass in all necessary arguments.
+      display_results(astronauts, test_titles, scores)
 
     elif index == 1:
       for title_index in range(len(test_titles)):
-        avg = # Call 'average_for_test' here. Pass in title_index and scores as arguments.
+        # Call 'average_for_test' here. Pass in title_index and scores as arguments.
+        avg = averages.average_for_test(title_index, scores)
         print("{0} test average = {1}%".format(test_titles[title_index], avg))
     elif index == 2:
       for astronaut_index in range(len(astronauts)):
-        avg = # Call 'average_for_student' here. Pass in astronaut_index and scores as arguments.
+        # Call 'average_for_student' here. Pass in astronaut_index and scores as arguments.
+        avg = averages.average_for_student(astronaut_index, scores)
         print("{0}'s test average = {1}%".format(astronauts[astronaut_index], avg))
     else:
-      walker = # Call 'random_from_list' to pick a spacewalker from the astronauts list.
-      print(walker + " is the next spacewalker.")
+      # Call 'random_from_list' to pick a spacewalker from the astronauts list.
+      walker = random_select.random_from_list(astronauts)
+
+      print("\n" + walker + " is the next spacewalker.")
   else:
     print("Option skipped.")
--- random_select.py
+++ random_select.py
@@ -2,5 +2,6 @@ import random
 
 def random_from_list(a_list):
   # Your code here to select a random element from the list passed to the function.
-  
-  return # Return the selected element
+  random_index = random.randint(0, len(a_list))
+  # Return the selected element
+  return a_list[random_index]
